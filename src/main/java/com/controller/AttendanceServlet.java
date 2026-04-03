fbc624819bbf27008c30fb754aa15b7cf9879dda
rameshkunchala <kunchalaramesh123@gmail.com>
2026-04-03 19:04:05 +0530

    Updated servlet package

--- src/main/java/com/controller/AttendanceServlet.java
+++ src/main/java/com/controller/AttendanceServlet.java
@@ -6,7 +6,6 @@ import com.app.service.AttendanceService;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.sql.Date;
 
 @WebServlet("/markAttendance")
 public class AttendanceServlet extends HttpServlet {
@@ -14,20 +13,18 @@ public class AttendanceServlet extends HttpServlet {
     private AttendanceService service = new AttendanceService();
 
     @Override
-    
-    @Override
-protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    System.out.println("=== AttendanceServlet HIT ===");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("=== AttendanceServlet HIT ===");
 
-    Attendance attendance = new Attendance(
-            101,
-            "Ramesh",
-            java.sql.Date.valueOf("2026-04-03"),
-            "Present"
-    );
+        Attendance attendance = new Attendance(
+                101,
+                "Ramesh",
+                java.sql.Date.valueOf("2026-04-03"),
+                "Present"
+        );
 
-    service.markAttendance(attendance);
+        service.markAttendance(attendance);
 
-    resp.sendRedirect(req.getContextPath() + "/report.jsp");
-}
+        resp.sendRedirect(req.getContextPath() + "/report.jsp");
+    }
 }
