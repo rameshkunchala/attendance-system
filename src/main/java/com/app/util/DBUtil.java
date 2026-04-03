509339347d33232af47608f8f254d59f01edeac3
rameshkunchala <kunchalaramesh123@gmail.com>
2026-04-03 19:34:01 +0530

    Testinf loging with harcoded values

--- src/main/java/com/app/util/DBUtil.java
+++ src/main/java/com/app/util/DBUtil.java
@@ -1,38 +1,22 @@
 package com.app.util;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class DBUtil {
 
     public static Connection getConnection() {
         try {
-            Properties props = new Properties();
-
-            InputStream input = DBUtil.class.getClassLoader()
-                    .getResourceAsStream("db.properties");
-
-            if (input == null) {
-                throw new RuntimeException("db.properties not found in classpath");
-            }
-
-            props.load(input);
-
-            String url = props.getProperty("db.url");
-            String user = props.getProperty("db.username");
-            String password = props.getProperty("db.password");
-
-            System.out.println("URL = " + url);
-            System.out.println("USER = " + user);
+            String url = "jdbc:mysql://localhost:3306/attendance_db";
+            String user = "root";
+            String password = "Root@12345";
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            System.out.println("Trying DB connection...");
             return DriverManager.getConnection(url, user, password);
 
         } catch (Exception e) {
-            System.out.println("=== DB CONNECTION FAILED ===");
             e.printStackTrace();
             throw new RuntimeException("DB connection failed", e);
         }
